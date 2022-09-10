998d4e7d94677389143c925fef0fae3dce2c66b8
Eveline Sofia Martinez Ayala (24133) <eveline.martinez@est.zamorano.edu>
2022-09-09 21:58:40 -0600

    Ejercicio 8.1

---
+++ lab01/lab01.py
@@ -0,0 +1,6 @@
+#tendencias e innovacion en tecnologia Agricola (TEA)
+#Autor: eveline.martinez@est.zamorano.edu
+#Fecha: 2022.09.02
+#Editado: 2022.09.02
+
+print ("Hola mundo!!!")
---
+++ lab02/imprimirnombre.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+nombre = input("Cual es tu nombe?")
+print("Hola" + nombre)
+edad = input("Cual es su edad")
+calculo = int(edad) * 10
+print(calculo)
